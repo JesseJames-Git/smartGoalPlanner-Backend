e3b284fb3a67face649c9ac466b3c4b4077c6e22
Jesse <jessejamesjj007@gmail.com>
2025-07-29 13:01:49 +0300

    Add Changes

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const jsonServer = require('json-server')
-const path = require('path')
-const server = jsonServer.create()
-const router = jsonServer.router("db.json")
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3000
 
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
-server.use('/api', router)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`)
-})
+});
